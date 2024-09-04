cdc2f0fd2af8a56a1a8d4f9634f406cdd279b669
Tommy AKONGUINA <tommy.akonguina@yahoo.fr>
2024-09-04 14:43:49 +0200

    transition hamburger

--- src/components/header/Hamburger.jsx
+++ src/components/header/Hamburger.jsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 import "./Hamburger.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 const Hamburger = () => {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   return (
     <div className="hamburger-container">
-      <GiHamburgerMenu
-        className="icon-menu"
-        size={48}
-        onClick={() => {
-          setHamburgerOpen(!hamburgerOpen);
-        }}
-      />
-      <div
-        className="hamburger-content"
-        style={{
-          display: `${hamburgerOpen ? "flex" : "none"}`,
-          backgroundColor: "blue",
-          position: "absolute",
-          top: "80px",
-          left: "0",
-        }}
-      >
+      {hamburgerOpen ? (
+        <GrClose
+          className="icon-menu"
+          size={40}
+          onClick={() => {
+            setHamburgerOpen(!hamburgerOpen);
+          }}
+        />
+      ) : (
+        <GiHamburgerMenu
+          className="icon-menu"
+          size={48}
+          onClick={() => {
+            setHamburgerOpen(!hamburgerOpen);
+          }}
+        />
+      )}
+
+      <div className={`hamburger-content ${hamburgerOpen ? "show" : ""}`}>
         <div className="burger">Connectes toi !</div>
         <div className="burger">Personnages</div>
         <div className="burger">Comics</div>
