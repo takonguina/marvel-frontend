637de918fa1952c5ddddf61546147d02d64b7280
Tommy AKONGUINA <tommy.akonguina@yahoo.fr>
2024-09-06 23:20:53 +0200

    starting description character

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -5,25 +5,41 @@ import "./Character.css";
 
 const Character = () => {
   const { id } = useParams();
-  const [data, setData] = useState({});
+  const [data, setData] = useState();
+  // const [comicsList, setComicsList] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const handleCharacter = async () => {
+    // const handleCharacter = async () => {
+    //   try {
+    //     const response = await axios.post(
+    //       `${import.meta.env.VITE_API}/characters/${id}`
+    //     );
+    //     if (response.status === 200) {
+    //       setData(response.data);
+    //       setIsLoading(false);
+    //     }
+    //     console.log(response.data.thumbnail);
+    //   } catch (error) {
+    //     error.response.message;
+    //   }
+    // };
+    const handleComicsList = async () => {
       try {
-        const response = await axios.post(
-          `${import.meta.env.VITE_API}/characters/${id}`
+        const response = await axios.get(
+          `${import.meta.env.VITE_API}/comics/${id}`
         );
         if (response.status === 200) {
           setData(response.data);
           setIsLoading(false);
         }
-        console.log(response.data.thumbnail);
       } catch (error) {
-        error.response.message;
+        console.log(error.response.message);
       }
     };
-    handleCharacter();
+
+    setIsLoading(true);
+    handleComicsList();
   }, [id]);
 
   return (
@@ -33,12 +49,29 @@ const Character = () => {
           <p>Loading</p>
         ) : (
           <div className="character-infos">
-            <img
+            {/* <img
               className="character-img-page"
               src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
               alt=""
-            />
-            <p>{data.name}</p>
+            /> */}
+            <div
+              className="character-img-page"
+              style={{
+                backgroundImage: `url(${data.thumbnail.path}.${data.thumbnail.extension})`,
+              }}
+            >
+              <div className="linear-gradient"></div>
+              <p>{data.name}</p>
+            </div>
+            <div className="description-character">
+              {data && data.description ? data.description : "Hero"}
+            </div>
+            <div className="character-comics-mentions">
+              {data &&
+                data.comics.map((comic, index) => {
+                  return <span key={index}>{comic.title}</span>;
+                })}
+            </div>
           </div>
         )}
       </div>
